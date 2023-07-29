ee3c0386f7c323fe4294b87e6c1aadf531f4b581
Antonio-David-Tejada-Villalon <4vdel777@gmail.com>
2023-07-29 02:25:14 -0300

    fixing likes 1

--- allNews/iLike.js
+++ allNews/iLike.js
@@ -1,11 +1,24 @@
-// Función para obtener los datos de "Me gusta" almacenados en el LocalStorage
-function getLikes() {
-    return JSON.parse(localStorage.getItem('likes')) || { count: 0, hasLiked: false };
+// Función para obtener el valor de una cookie por su nombre
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
   }
   
-  // Función para guardar los datos de "Me gusta" en el LocalStorage
+  // Función para establecer una cookie
+  function setCookie(name, value) {
+    document.cookie = `${name}=${value}; path=/`;
+  }
+  
+  // Función para obtener los datos de "Me gusta" almacenados en la cookie
+  function getLikes() {
+    const likesCookie = getCookie('likes');
+    return likesCookie ? JSON.parse(likesCookie) : { count: 0, hasLiked: false };
+  }
+  
+  // Función para guardar los datos de "Me gusta" en la cookie
   function saveLikes(likes) {
-    localStorage.setItem('likes', JSON.stringify(likes));
+    setCookie('likes', JSON.stringify(likes));
   }
   
   // Función para actualizar el botón de "Me gusta"
